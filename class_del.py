dc59120940e307f38e311c94ef1dd4848010951c
larago <bingeye@foxmail.com>
2017-01-27 16:12:52 +0800

    usage of __del__ of python's class

--- class_del.py
+++ class_del.py
@@ -1,2 +1,14 @@
 # encoding=utf8
 
+from os.path import join
+
+class FileObject(object):
+
+    def __init__(self, filePath='', fileName='sample.txt'):
+        self.file = open(join(filePath, fileName), 'r+')
+
+    def __del__(self):
+        self.file.close()
+        del self.file
+
+FileObject()
